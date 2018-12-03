37d9109259e17a16d59b82a02c0636b029b7ec17
miguel <miguel.contrerastorrejon@gmail.com>
2018-12-03 19:14:32 -0300

    ADD: ejercicos hash

---
+++ ejercicio1.rb
@@ -0,0 +1,3 @@
+#ejercicio1.rb
+h = { 'claveuno' => 10, 'clavedos' => 20, 'clavetres' => 30 }
+puts h
---
+++ ejercicio2.rb
@@ -0,0 +1,8 @@
+#Ejercicio2
+productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
+
+#y se realiza la siguiente consulta para conocer los productos existentes:
+productos.each { |valor, producto| puts producto }
+
+#2. Se quiere agregar un nuevo producto al hash:
+producto[2200] = cereal
